7cd4f83c9a308927ffbd789d446d9d248863495b
Daniel Wesley C <danielwesleyc@Daniels-MacBook-Air.local>
2024-08-30 10:46:39 +0530

    second commit

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 
 from django.db import models
+import datetime
+
 
 class Transaction(models.Model):
     date = models.DateField()
@@ -22,7 +24,26 @@ class PaymentDetail(models.Model):
 
     def __str__(self):
         return f"PaymentDetail {self.date} - {self.type_of_user} - {self.amount}"
-    
+
+from django.db import models
+import datetime
+
+class ticket_number(models.Model):
+    ticket_number = models.CharField(max_length=20, unique=True, blank=True)
+    date = models.DateField(auto_now_add=True)
+    time = models.TimeField(auto_now_add=True)
+    # Other fields...
+
+    def save(self, *args, **kwargs):
+        if not self.ticket_number:
+            self.ticket_number = self.generate_ticket_number()
+        super(ticket_number, self).save(*args, **kwargs)
+
+    def generate_ticket_number(self):
+        current_date = datetime.date.today().strftime("%Y%m%d")
+        current_time = datetime.datetime.now().strftime("%H%M%S")
+        return f"TKT{current_date}{current_time}"
+
     
     
     
