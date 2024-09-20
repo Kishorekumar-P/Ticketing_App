f204eebc508486d1e6c34821d77b03a0d68534da
Pranavarun05 <111700060+kishorekumarpandian@users.noreply.github.com>
2024-09-20 23:32:31 +0530

    5th commit

--- app/urls.py
+++ app/urls.py
@@ -2,15 +2,11 @@
 from django.urls import path
 from app import views
 
-
-
 app_name = "app"
 
 urlpatterns = [
    
    path('' , views.login_view , name="login"),
    path('home' , views.home_view , name="home") , 
-   path ('collection_r' , views.collection_report_view , name = "collection") ,
-   path('detail' , views.payment_detail_view , name="detail"),
-
+   path ('collection_report' , views.collection_report , name = "collection_report") ,
 ]
--- app/views.py
+++ app/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.contrib import messages
-from app.models import Transaction, PaymentDetail
 from app.models import Ticket
-from django.db import transaction
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
 
@@ -43,7 +41,7 @@ def home_view(request):
             # Save data in the database
             ticket = Ticket(
                 adult_count=adult_count,
-                children_count=children_count,
+                children_count=children_count, 
                 student_count=student_count,
                 total_amount=total_amount,
                 payment_type=payment_type,
@@ -60,67 +58,8 @@ def home_view(request):
     return render(request, 'home.html')
 
 @login_required(login_url='/login')
-def collection_report_view(request):
-    transactions = Transaction.objects.all().order_by('-date')
+def collection_report(request):
+    collection_report = Ticket.objects.all()
+    return render(request, 'collection_report.html', {'collection_report': collection_report})
 
-    report_data = [
-        {
-            'date': transaction.date.strftime('%d-%m-%Y'),
-            'payment_type': transaction.payment_type,
-            'total_amount': transaction.total_amount
-        }
-        for transaction in transactions
-    ]
 
-    return render(request, 'collection_r.html', {'report_data': report_data})
-
-@login_required(login_url='/login')
-def payment_detail_view(request):
-    payment_details = PaymentDetail.objects.values('date', 'type_of_user').annotate(
-        count=Sum('amount') / 50,  
-        total_amount=Sum('amount')
-    ).order_by('date')
-    report_data = {}
-
-    for detail in payment_details:
-        date = detail['date'].strftime('%d-%m-%Y')
-        user_type = detail['type_of_user']
-        count = int(detail['count'])
-        total_amount = detail['total_amount']
-
-        if date not in report_data:
-            report_data[date] = {
-                'child_count': 0,
-                'child_total_amount': 0,
-                'student_count': 0,
-                'student_total_amount': 0,
-                'children_count': 0,
-                'children_total_amount': 0,
-            }
-
-        if user_type == 'child':
-            report_data[date]['child_count'] = count
-            report_data[date]['child_total_amount'] = total_amount
-        elif user_type == 'student':
-            report_data[date]['student_count'] = count
-            report_data[date]['student_total_amount'] = total_amount
-        elif user_type == 'children':
-            report_data[date]['children_count'] = count
-            report_data[date]['children_total_amount'] = total_amount
-
-    report_data_list = [
-        {
-            'date': date,
-            'child_count': data['child_count'],
-            'child_total_amount': data['child_total_amount'],
-            'student_count': data['student_count'],
-            'student_total_amount': data['student_total_amount'],
-            'children_count': data['children_count'],
-            'children_total_amount': data['children_total_amount'],
-        }
-        for date, data in report_data.items()
-    ]
-
-    report_data_list.sort(key=lambda x: datetime.strptime(x['date'], '%d-%m-%Y'), reverse=True)
-
-    return render(request, 'payment_detail.html', {'report_data': report_data_list})
