8da37a2c1577a3e0fc1168f9c820aae9214ba4ac
Pranavarun05 <111700060+kishorekumarpandian@users.noreply.github.com>
2024-09-23 15:59:28 +0530

    6th commit

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,6 @@ from app import views
 app_name = "app"
 
 urlpatterns = [
-   
    path('' , views.login_view , name="login"),
    path('home' , views.home_view , name="home") , 
    path ('collection_report' , views.collection_report , name = "collection_report") ,
